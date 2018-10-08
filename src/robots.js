ef6f90dd5fc58ac17de3e491d58f4de6e1b8b87b
Bobby <33196341+shdow@users.noreply.github.com>
2018-10-08 17:20:20 +0300

    test

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'John TheLegend',
+    username: 'johnthelegend',
+    email: 'johnthelegend@john.com',
+  },
+  {
+    id: 2,
+    name: 'Jane DOC',
+    username: 'janedoc',
+    email: 'janedoc@jane.com',
+  },
+  {
+    id: 3,
+    name: 'Mr Target',
+    username: 'mrtarget',
+    email: 'mrtarget@mrt.com'
+  },
+  {
+    id: 4,
+    name: 'Fred Grosvenor',
+    username: 'FredGrosvenor',
+    email: 'fredgrosvenor@fred.com'
+  },
+  {
+    id: 5,
+    name: 'Fabiana Dixie',
+    username: 'FabianaDixie',
+    email: 'Fabiana.Dixie@dixie.com',
+  },
+  {
+    id: 6,
+    name: 'Jayden Walsh',
+    username: 'JaydenWalsh',
+    email: 'Jayden.Walsh@jayden.com'
+  },
+  {
+    id: 7,
+    name: 'Henry Holland',
+    username: 'HenryHolland',
+    email: 'Henry.Holland@henry.com'
+  },
+  {
+    id: 8,
+    name: 'Aidan Cooper',
+    username: 'AidanCooper',
+    email: 'Aidan.Cooper@cooper.com'
+  },
+  {
+    id: 9,
+    name: 'Billy Perry',
+    username: 'BillyPerry',
+    email: 'Billy.Perry@billy.com'
+  },
+  {
+    id: 10,
+    name: 'Layton Martinez',
+    username: 'LaytonMartinez',
+    email: 'Layton.Martinez@leyton.com'
+  }
+]
